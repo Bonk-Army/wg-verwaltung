2cca4691df02c0af7cf3cc702f9ad53700fc3cbc
David <D.Huh@t-online.de>
2020-06-04 13:02:16 +0200

    :sparkles: Added LoginBean
    - Ticket: WGV-42

---
+++ src/beans/LoginBean.java
@@ -0,0 +1,25 @@
+package beans;
+
+// Imports
+// import models.User;
+
+public class LoginBean {
+    //Variables
+
+        // private User user;
+
+    //Public Methods
+
+        // checkLogin() { SQL Query: Try to find combination of username and password }
+        // Check if username and password is in SQL
+            // true -> user = user
+            // else -> return "Wrong combination of username and password"
+
+        // createUser() { new User = User(username, password) -> merge to sql}
+
+        // sendPassword() { mail service }
+
+    //Getter and Setter
+
+        // getUser() { return this.User; }
+}
