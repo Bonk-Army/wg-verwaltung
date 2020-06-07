b935b47ba9d2f35f54a2430c9afece23cf6bce15
Patrick Müller <patrick@plutodev.de>
2020-06-07 21:04:27 +0200

    WGV-37: Updated pom.xml to exclude the old, unused libs and started to write accessor sql methods

--- src/main/java/utilities/SQLDatabaseConnection.java
+++ src/main/java/utilities/SQLDatabaseConnection.java
@@ -1,21 +1,50 @@
 package utilities;
 
 import java.sql.*;
-
-import com.jcraft.jsch.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLDatabaseConnection {
     public static void main(String[] args) {
+        System.out.println(getUsers());
+    }
+
+    public static List<String> getUsers(){
+        List<String> retList = new ArrayList<String>();
+
+        ResultSet rs = executeQuery("SELECT username FROM users");
+
+        try {
+            while (rs.next()) {
+                retList.add(rs.getString(1));
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return retList;
+    }
+
+
+    /**
+     * Executes a given sql query and returns the ResultSet
+     * @param query The desired query
+     * @return The ResultSet
+     */
+    private static ResultSet executeQuery(String query){
+        ResultSet returnSet = null;
+
         try {
             Connection con = DriverManager.getConnection(
                     ("jdbc:mariadb://v220190910299696193.nicesrv.de:3306/wg_verwaltung?user=wg_admin&password=" + SecretsReader.getSqlPassword()));
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from users");
-            while (rs.next())
-                System.out.println(rs.getString(1));
+            ResultSet rs = stmt.executeQuery(query);
+            returnSet = rs;
             con.close();
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        return returnSet;
     }
 }
