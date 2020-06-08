3fc5f2c1f3bfe1502a7974a110c7303d27f00393
Nico <ichBinFuerMehrDatenschutz@NichtVorhanden.de>
2020-06-08 11:30:16 +0200

    WGV-70  Moved config to * Package

--- src/main/java/view/parts/ContentPart.java
+++ src/main/java/view/parts/ContentPart.java
@@ -1,11 +1,5 @@
 package view.parts;
 
-import view.config.globalConfig;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-
 abstract public class ContentPart extends Part{
 
     protected String content = "";
