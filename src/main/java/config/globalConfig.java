cefa404e2b0204721c63a647e51e4d0c0db35f7f
Patrick Müller <patrick@plutodev.de>
2020-06-09 13:13:12 +0200

    :alembic: Advanced testing environment detection right here

--- src/main/java/config/globalConfig.java
+++ src/main/java/config/globalConfig.java
@@ -6,16 +6,17 @@ public class globalConfig {
     /**
      * Tries to read value from environment variable. If variable is not known (so we are not on Azure), it returns
      * true. If the env var exists (so we are on azure), it returns true
+     *
      * @return If this is a testing environment
      */
-    public static boolean isTest(){
-        if(System.getenv("IS_AZURE") == null){
+    public static boolean isTest() {
+        if (System.getenv("IS_AZURE") == null) {
             return true;
         }
 
         return false;
     }
 
-    public globalConfig(){
+    public globalConfig() {
     }
 }
