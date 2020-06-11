593445146b40bcf91985e24a7ff9495712b7251f
Patrick Müller <patrick@plutodev.de>
2020-06-11 14:44:50 +0200

    Fixing issue with email regex

--- src/main/java/utilities/RegexHelper.java
+++ src/main/java/utilities/RegexHelper.java
@@ -8,15 +8,25 @@ public class RegexHelper {
     /**
      * Checks if the entered String matches the rules we have for usernames.
      * Not allowed are whitespaces and ;?&%' (; because we dont want SQLi)
+     *
      * @param username The username to be matched
      * @return If the username is allowed
      */
-    public static boolean checkUsername(String username){
+    public static boolean checkUsername(String username) {
         return username.matches("^[^;?&%'\\s]*$");
     }
 
+    /**
+     * Checks if the entered String matches the rules we have for email addresses.
+     * Not allowed are whitespaces and ;?&%'
+     *
+     * @param email The email address String to be matched
+     * @return If the email address is allowed
+     */
     public static boolean checkEmail(String email) {
-        if (!email.matches("^[^;?&%'\\s]*$")) {
+        //Check for illegal characters
+        if (email.matches("^[^;?&%'\\s]*$")) {
+            //Check for corret email address format
             if (email.matches(".+@.+\\..+")) {
                 return true;
             }
