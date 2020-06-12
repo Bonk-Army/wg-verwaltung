40bd02a25c0ed07eb02922de9a279ef64830b3af
David <D.Huh@t-online.de>
2020-06-12 12:26:55 +0200

    :sparkles: Added SQLDCTodo
    - Ticket: WGV-89

---
+++ src/main/java/utilities/SQLDCTodo.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import java.util.Date;
+import java.sql.ResultSet;
+
+public class SQLDCTodo extends SQLDatabaseConnection {
+    /**
+     * Create a to-do in the database
+     *
+     * @param task  The task of the user
+     * @param userId    The ID of the user
+     * @param wgId  The ID of the WG
+     * @param dateDue The date til the task should be done
+     * @return If the to-do has been created successfully. If not, the user has to be informed!
+     */
+    public static boolean createTodo(String task, String userId, String wgId, Date dateDue) {
+        try {
+            Date dateCreated = new Date();
+            ResultSet rs = executeQuery(("INSERT INTO todo (task, userId, wgId, dateCreated, dateDue, isDone)"
+                    + "VALUES ('" + task + "', '" + userId + "', '" + wgId + "', '" + dateCreated + "', '" + dateDue + "', '" + false + "')"));
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
