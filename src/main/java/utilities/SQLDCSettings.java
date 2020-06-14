e546abf230d5771e4528b4529b8bd15a6d7f01a4
David <D.Huh@t-online.de>
2020-06-14 11:30:20 +0200

    :sparkles: Added method "getWgId" to SQLDCSettings
    - Ticket: WGV-93

--- src/main/java/utilities/SQLDCSettings.java
+++ src/main/java/utilities/SQLDCSettings.java
@@ -10,18 +10,37 @@ public class SQLDCSettings extends SQLDatabaseConnection {
      * @param accessKey the access key of the wg
      * @return If the to-do has been created successfully. If not, the user has to be informed!
      */
-
     public static boolean createWg(String name, String accessKey) {
         try {
-            ResultSet rs = executeQuery(("INSERT INTO wgs (name, accessKey)"
-                    + "VALUES ('" + name + "', '" + accessKey + "')"));
-
-            return true;
+            if (RegexHelper.checkUsername(name)) {
+                ResultSet rs = executeQuery(("INSERT INTO wgs (name, accessKey)"
+                        + "VALUES ('" + name + "', '" + accessKey + "')"));
+                return true;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return false;
     }
+
+    public static String getWgId(String accessKey) {
+        String wgId = "";
+
+        try {
+            if (RegexHelper.checkUsername(accessKey)) {
+                ResultSet rs = executeQuery(("SELECT uniqueID FROM wgs WHERE accessKey='" + accessKey + "'"));
+                while (rs.next()) {
+                    wgId = rs.getString(1);
+                }
+                return wgId;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
 
 }
