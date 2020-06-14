08d311fa61d1f9ea8e6c03349993c633979becb4
David <D.Huh@t-online.de>
2020-06-14 11:16:16 +0200

    :sparkles: Added SQLDCSettings
    - Ticket: WGV-93

---
+++ src/main/java/utilities/SQLDCSettings.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import java.sql.ResultSet;
+
+public class SQLDCSettings extends SQLDatabaseConnection {
+    /**
+     * Create a wg in the database
+     *
+     * @param name      the name of the wg
+     * @param accessKey the access key of the wg
+     * @return If the to-do has been created successfully. If not, the user has to be informed!
+     */
+
+    public static boolean createWg(String name, String accessKey) {
+        try {
+            ResultSet rs = executeQuery(("INSERT INTO wgs (name, accessKey)"
+                    + "VALUES ('" + name + "', '" + accessKey + "')"));
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
--- src/main/java/utilities/SQLDCTodo.java
+++ src/main/java/utilities/SQLDCTodo.java
@@ -7,9 +7,9 @@ public class SQLDCTodo extends SQLDatabaseConnection {
     /**
      * Create a to-do in the database
      *
-     * @param task  The task of the user
-     * @param userId    The ID of the user
-     * @param wgId  The ID of the WG
+     * @param task    The task of the user
+     * @param userId  The ID of the user
+     * @param wgId    The ID of the WG
      * @param dateDue The date til the task should be done
      * @return If the to-do has been created successfully. If not, the user has to be informed!
      */
