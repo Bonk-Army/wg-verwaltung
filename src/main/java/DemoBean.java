60e520589fc1a0a809bfbc472e857a415138666f
Nico <ichBinFuerMehrDatenschutz@NichtVorhanden.de>
2020-06-14 14:44:37 +0200

    WGV-91 Added initial new JSP HURENSOHN Filestructure

--- src/main/java/DemoBean.java
+++ src/main/java/DemoBean.java
@@ -1,7 +1,16 @@
+import java.util.Date;
+
 public class DemoBean {
 
     public String username = "username";
+    public int kontoguthaben = 100;
+    public String vorname = "Max";
+    public String nachname = "Mustermann";
+    Date date;
+    public String todo;
 
-
-
+    public DemoBean(){
+        date = new Date();
+         todo = "Wisch raus du Alpaka!;Nico;"+date.toString()+";";
+    }
 }
