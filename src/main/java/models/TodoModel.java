4c73c616560654524dd956baecac89b8b1a2df37
Patrick Müller <patrick@plutodev.de>
2020-06-15 10:57:15 +0200

    Generated Getter for TodoModel

--- src/main/java/models/TodoModel.java
+++ src/main/java/models/TodoModel.java
@@ -6,12 +6,12 @@ import java.util.Date;
  * Model for To-Do entry
  */
 public class TodoModel {
-    String task;
-    String userId;
-    String wgId;
-    Date dateCreated;
-    Date dateDue;
-    Boolean isDone;
+    private String task;
+    private String userId;
+    private String wgId;
+    private Date dateCreated;
+    private Date dateDue;
+    private Boolean isDone;
 
     public TodoModel(String task, String userId, String wgId, Date dateCreated, Date dateDue, Boolean isDone) {
         this.task = task;
@@ -20,5 +20,29 @@ public class TodoModel {
         this.dateCreated = dateCreated;
         this.dateDue = dateDue;
         this.isDone = isDone;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getWgId() {
+        return wgId;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getDateDue() {
+        return dateDue;
+    }
+
+    public Boolean getDone() {
+        return isDone;
     }
 }
