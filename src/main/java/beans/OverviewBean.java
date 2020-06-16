4177b6cdbb4ede356bf2ac161bbd31f35abb6e2b
henningxtro <henning.sextro@gmail.com>
2020-06-16 17:03:40 +0200

    WGV-112 Added JavaDoc to OverviewBean

--- src/main/java/beans/OverviewBean.java
+++ src/main/java/beans/OverviewBean.java
@@ -7,11 +7,24 @@ public class OverviewBean {
     // Variables
 
     // Public Methods
+    /**
+     * Get Total number of To-Do's
+     * Done or not!
+     *
+     * @param wgID  The ID of the WG
+     * @return Number of To-Do's for given WG
+     */
     public int getNumberofTodos(String wgID) {return SQLDCOverview.countTodo(wgID); }
+
+    /**
+     * Get number of done To-Do's
+     *
+     * @param wgID The ID of the WG
+     * @return Number of done To-Do's for given WG
+     */
     public int getNumberofDone(String wgID) {return SQLDCOverview.countDone(wgID); }
     }
 
     // Private Methods
 
     // Getter and Setter
-}
