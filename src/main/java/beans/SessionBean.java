0152f7e0ad2bab73105cfc25149bca7929f6a025
Patrick Müller <patrick@plutodev.de>
2020-06-18 20:15:18 +0200

    WGV-119: Made sessionBean SessionScoped because thats the smart thing to do

--- src/main/java/beans/SessionBean.java
+++ src/main/java/beans/SessionBean.java
@@ -1,6 +1,13 @@
 package beans;
 
-public class SessionBean {
+import javax.enterprise.context.SessionScoped;
+
+/**
+ * Session bean declared as SessionScoped by default because we need it during the whole session.
+ * Must implement java.io.Serializable.
+ */
+@SessionScoped
+public class SessionBean implements java.io.Serializable{
     private String userId = "";
 
     public SessionBean(String userId) {
