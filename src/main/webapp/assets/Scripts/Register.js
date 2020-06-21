78cf629bde1e3053aaa419c1f85d78072f3a7fde
Krissi <blauerelefant005@gmail.com>
2020-06-21 22:47:28 +0200

    WGV-92 Adapt setting page

--- src/main/webapp/assets/Scripts/Register.js
+++ src/main/webapp/assets/Scripts/Register.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#visibleRegisterButton').on('click', function () {
         let password = document.querySelector('#password').value;
         let repeatedPassword = document.querySelector('#password2').value;
-        if(password === repeatedPassword){
+        if(password === repeatedPassword && password !== ""){
             $('#registerButton').click();
         } else {
             window.alert(unescape("Deine Passw%F6rter sind nicht identisch.%0A"));
--- src/main/webapp/assets/Scripts/Settings.js
+++ src/main/webapp/assets/Scripts/Settings.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#visibleSavePasswordButton').on('click', function () {
         let password = document.querySelector('#password').value;
         let repeatedPassword = document.querySelector('#password2').value;
-        if(password === repeatedPassword){
+        if(password === repeatedPassword && password !== ""){
             $('#savePasswordButton').click();
         } else {
             window.alert(unescape("Deine Passw%F6rter sind nicht identisch.%0A"));
