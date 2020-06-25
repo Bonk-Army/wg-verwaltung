ea594a2dfcf7e534954859bda70f7005f9b71441
Patrick Müller <patrick@plutodev.de>
2020-06-25 13:06:09 +0200

    WGV-141: Fixing bug where a logout after a while caused an error

--- src/main/java/logic/Logout.java
+++ src/main/java/logic/Logout.java
@@ -24,31 +24,50 @@ public class Logout extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         SessionBean sessionBean = (SessionBean) request.getSession().getAttribute("sessionBean");
 
-        // Log the user out of the session bean
-        ErrorCodes status = sessionBean.logout();
-
-        // Delete the session cookie
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("session")) {
-                    cookie.setValue("");
-                    cookie.setPath("/");
-                    cookie.setMaxAge(0);
-                    response.addCookie(cookie);
+        // If session bean does not exist, it already got cleared and the user is logged out.
+        // Just delete the cookie and redirect to login page
+        if (sessionBean == null) {
+            // Delete the session cookie
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("session")) {
+                        cookie.setValue("");
+                        cookie.setPath("/");
+                        cookie.setMaxAge(0);
+                        response.addCookie(cookie);
+                    }
                 }
             }
-        }
 
-        switch (status) {
-            case SUCCESS:
-                //Redirect to login page
-                response.sendRedirect("/");
-                break;
-            case FAILURE:
-                //Show failure
-                request.getServletContext().getRequestDispatcher("/responseFailure").forward(request, response);
-                break;
+            response.sendRedirect("/");
+        } else {
+            // Log the user out of the session bean
+            ErrorCodes status = sessionBean.logout();
+
+            // Delete the session cookie
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("session")) {
+                        cookie.setValue("");
+                        cookie.setPath("/");
+                        cookie.setMaxAge(0);
+                        response.addCookie(cookie);
+                    }
+                }
+            }
+
+            switch (status) {
+                case SUCCESS:
+                    //Redirect to login page
+                    response.sendRedirect("/");
+                    break;
+                case FAILURE:
+                    //Show failure
+                    request.getServletContext().getRequestDispatcher("/responseFailure").forward(request, response);
+                    break;
+            }
         }
     }
 
