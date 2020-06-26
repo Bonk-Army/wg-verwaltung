d4f7caf8adf8b9d833b0c14368ac0b2bf35d6dfa
Patrick Müller <patrick@plutodev.de>
2020-06-26 16:52:58 +0200

    WGV-138: Fixing wrong time zone

--- src/main/java/utilities/DateFormatter.java
+++ src/main/java/utilities/DateFormatter.java
@@ -2,7 +2,9 @@ package utilities;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Utility class used to format a date to a localized date string
@@ -18,5 +20,12 @@ public class DateFormatter {
     public static String dateToString(Date date){
         SimpleDateFormat formatter = new SimpleDateFormat("dd. MMMM yyyy");
         return formatter.format(date);
+    }
+
+    public static Date getCurrentDateTime(){
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"));
+        Date currentDate = calendar.getTime();
+
+        return currentDate;
     }
 }
